d40c8d72b4107760b2055af25b335e8f9278f04e
Ayesha Bhuiyan <abhuiyan122@gmail.com>
2024-07-31 16:36:15 +0000

    js examples

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 
 //console log text, expression 
-
+// console.log("Hello world")
+// console.log(1 + 2)
 // Create a variable to hold your name as a string
-
+// let myName = "Ayesha"
+// myName = "Bhuiyan"
+// console.log(myName)
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!” 
-
+// const myMessage = "Hello, "
+// console.log(myMessage + myName)
 //console log your name variable
 
 //Re-assign the value of your name variable below its declaration and console log it again
 
 //practice math operators
+// console.log(10-7)
+// console.log(10*7)
+// console.log(20/10)
+// console.log(15%10)
 
 // boolean - true or false
+// let isSunny = true
+//console.log(isSunny)
+// console.log(!isSunny)
 
 //create null and undefined
+// let isHungry
+// console.log(isHungry)
 
 //mix data types
+// let num = "12"
+// console.log(typeof num)
+// num = Number(num)
+// console.log(typeof num)
 
 //string methods
 
